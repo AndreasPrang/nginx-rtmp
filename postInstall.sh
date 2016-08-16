b21edd4503fa4b65df3b3cbdd092cf31ef01c461
Prang, Andreas <andreas.prang@bild.de>
2016-08-16 22:40:50 +0200

    added parameter for Development

--- postInstall.sh
+++ postInstall.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 service nginx start
+
+# PRODUCTION
 /opt/letsencrypt/letsencrypt-auto certonly --agree-tos --text --non-interactive --register-unsafely-without-email -a webroot --webroot-path=/var/www/ -d stream.demokratielive.org
+# DEVELOPMENT
+# /opt/letsencrypt/letsencrypt-auto certonly --test-cert --agree-tos --text --non-interactive --register-unsafely-without-email -a webroot --webroot-path=/var/www/ -d stream.demokratielive.org
+
 cp /usr/local/nginx/conf/nginx.conf.new /usr/local/nginx/conf/nginx.conf
 
 service nginx restart
