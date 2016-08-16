0652d73bdc1fddd30e8ee5aa11cce0f355974f98
Prang, Andreas <andreas.prang@bild.de>
2016-08-16 11:29:24 +0200

    fixed nginx.conf

--- postInstall.sh
+++ postInstall.sh
@@ -2,4 +2,6 @@
 
 service nginx start
 /opt/letsencrypt/letsencrypt-auto certonly --agree-tos --text --non-interactive --register-unsafely-without-email -a webroot --webroot-path=/var/www/ -d stream.demokratielive.org
+cp /usr/local/nginx/conf/nginx.conf.new /usr/local/nginx/conf/nginx.conf
+
 service nginx restart
